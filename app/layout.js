4e0c15cf3323f6d486456dab88fbf88fcb887dfb
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2023-07-11 04:02:53 +0100

    footer component icons updated

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,8 @@ export default function RootLayout({ children }) {
 		<html lang='en'>
 			<body>
 				<Navbar />
-        <Footer />
 				{children}
+        <Footer />
 			</body>
 		</html>
 	);
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,53 +1,66 @@
 import React from 'react'
-import { FaLocationDot, BsFacebook, FaTiktok, BsInstagram } from "react-icons/fi";
+import { FaLocationDot, FaSquareFacebook, FaInstagram, FaAt, FaPhone, FaTiktok } from "react-icons/fa6";
 import Link from 'next/link';
 
 const Footer = () => {
-  return (
-      <footer className='flex flex-wrap justify-between px-6 py-4 bg-[#000000] fixed left-0 bottom-0 w-full ' style={{ color: "#FFFFFF" }}>
-        <div><h3>Get In Touch</h3>
-              <div>
-               
-                  <p> Bon Accord AB11 6UA</p>
-                  <p> +44 7753662690</p>
-                  <p> hannahsguesthouse@gmail.com </p>
-              </div>
-        </div>
+    return (
+        <footer className='flex flex-wrap justify-between px-6 py-6 bg-[#000000] fixed left-0 bottom-0 w-full '>
+            <div className='opacity-50 py-4 px-4' >
+                <div className='font-bold text-3xl pb-4'><h3>Get In Touch</h3></div>
+                <div>
+                    <p className="flex items-center pb-3">
+                        <span className="mr-2"><FaLocationDot /></span>
+                        Bon Accord AB11 6UA
+                    </p>
 
-          <div className ='align-content: center;'>
-             <h3>Quick Links</h3>
-            <Link href = '/'> 
-                <h3>Home</h3>
-            </Link>
 
-            <Link href = 'rooms'>
-                <h3>Room</h3>
-            </Link>
+                    <p className='flex items-center pb-3'>
+                        <span className='mr-2'><FaPhone /></span>
+                        +44 7753662690
+                    </p>
 
-            <Link href = 'about'>
-                <h3>About</h3>
-            </Link>
-
-            <Link href = 'contact'>
-                <h3>Contact</h3>
-            </Link>
-
-        </div>
-
-        <div>
-            <h3>Our Social Media</h3>
-            <div>
-                  {/* <BsFacebook />
-                  <FaTiktok />
-                  <BsInstagram /> */}
+                    <p className='flex items-center'>
+                        <span className='mr-2'> <FaAt /></span>
+                        hannahsguesthouse@gmail.com
+                    </p>
+                </div>
             </div>
 
-              
-        </div>
+            <div className=' opacity-50 font-bold align-content: center;'>
+                <h3 className='font-bold text-3xl pb-4'>Quick Links</h3>
+                <Link href='/'>
+                    <h3 class="text-center">Home</h3>
+                </Link>
 
-        
-    </footer>
-  )
-}
+                <Link href='rooms'>
+                    <h3 class="text-center">Room</h3>
+                </Link>
 
-export default Footer
+                <Link href='about'>
+                    <h3 class="text-center">About</h3>
+                </Link>
+
+                <Link href='contact'>
+                    <h3 class="text-center">Contact</h3>
+                </Link>
+            </div>
+
+            <div>
+                <h3 className=' font-bold text-3xl  opacity-50 pb-4'>Our Social Media</h3>
+
+                <div class="flex justify-center">
+                    <div class="flex text-center gap-5">
+                        <span class="text-3xl"><FaSquareFacebook /></span>
+                        <span class="text-3xl"><FaInstagram /></span>
+                        <span class="text-3xl"><FaTiktok /></span>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </footer>
+    );
+};
+
+export default Footer;
