0861e0822b9ccfb9b892de7bdcfea40873946ab1
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2023-07-14 19:21:08 +0100

    redesigned guest rooms

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
 				<Navbar />
 				{children}
 				<Roomcard />
-        <Footer />
+        {/* <Footer /> */}
         {/* <Values /> */}
 			</body>
 		</html>
--- components/Roomcard.jsx
+++ components/Roomcard.jsx
@@ -1,61 +1,75 @@
-import React from 'react'
+import React from "react";
 
 const Roomcard = () => {
   return (
-      <div className='flex relative flex-wrap mb-4 px-56 items-center justify-center'>
-
-          <div className='w-1/2 '>
-            <div style = {{
-                backgroundImage:'url(/images/DeluxeK.jpg)',
-                backgroundSize: '400px',
-                backgroundRepeat:'no-repeat',
-                height:'400px',
-                width:'350px' 
+    <div className="w-full md:h-screen p-2 flex items-center text-center py-16">
+      <div className=" max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-1 max-w-sm rounded overflow-hidden shadow-lg">
+          <div
+            style={{
+              backgroundImage: "url(/images/DeluxeK.jpg)",
+              backgroundSize: "400px",
+              backgroundRepeat: "no-repeat",
+              height: "300px",
+              width: "350px",
             }}
-                className='rounded-md shadow-md'
-            >
-            </div>
-
-        </div>
-          <div className='w-1/2 py-20 -mx-12 '>
-            <div className='h-1/2 '>
-                  <div style={{
-                      backgroundImage: 'url(/images/king.jpg)',
-                      backgroundSize: '400px',
-                      backgroundRepeat: 'no-repeat',
-                      backgroundPosition:'50% 45%',
-                      height: '180px',
-                      width: '350px',
-                      paddingBottom:'20px',
-                  }}
-                      className='rounded-md shadow-md'
-                  >
-                  </div>
-
-
-            </div>
-              <div className='h-1/2 mt-10'>
-                  <div style={{
-                      backgroundImage: 'url(/images/single.jpg)',
-                      backgroundSize: '400px',
-                      backgroundRepeat: 'no-repeat',
-                      backgroundPosition: '50% 60%',
-                      height: '180px',
-                      width: '350px'
-                  }}
-                      className='rounded-md shadow-md '
-                  >
-                  </div>
-
-
-              </div>
             
+          ></div>
+          <div className='px-6 py-24'></div>
         </div>
-
-        
-        
+      </div>
     </div>
   );
 };
 
 export default Roomcard;
+
+//  <div className='col-span-1 '>
+
+//                   <h2 className='py-4  text-xl uppercase tracking-widdest text-left'>Room Types</h2>
+//                   <div style={{
+//                       backgroundImage: 'url(/images/DeluxeK.jpg)',
+//                       backgroundSize: '400px',
+//                       backgroundRepeat: 'no-repeat',
+//                       height: '400px',
+//                       width: '350px'
+//                   }}
+//                       className='rounded-md shadow-md'
+//                   >
+
+//                   </div>
+
+//               </div>
+//               <div className='col-span-1 grid-row-2  py-10 '>
+//                   <div className='row-span-1 '>
+//                       <div style={{
+//                           backgroundImage: 'url(/images/king.jpg)',
+//                           backgroundSize: '400px',
+//                           backgroundRepeat: 'no-repeat',
+//                           backgroundPosition: '50% 45%',
+//                           height: '180px',
+//                           width: '350px',
+
+//                       }}
+//                           className='rounded-md shadow-md'
+//                       >
+//                       </div>
+
+//                   </div>
+//                   <div className='row-span-1  py-6'>
+//                       <div style={{
+//                           backgroundImage: 'url(/images/single.jpg)',
+//                           backgroundSize: '400px',
+//                           backgroundRepeat: 'no-repeat',
+//                           backgroundPosition: '50% 60%',
+//                           height: '180px',
+//                           width: '350px'
+//                       }}
+//                           className='rounded-md shadow-md  '
+//                       >
+//                       </div>
+
+//                   </div>
+//                   <h1 className='py-4 tracking-widdest text-right text-l text-gray-500 underline cursor-pointer'>See More</h1>
+
+//               </div>
