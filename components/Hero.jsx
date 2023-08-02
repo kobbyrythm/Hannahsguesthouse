0456e2368849f8c1f6c45962b3c4f186d416740d
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2023-08-02 02:15:06 +0100

    tweaks

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -66,44 +66,48 @@ const Hero = () => {
 
 
     return (
-        <div className='max-w-full h-[700px] w-full m-auto pb-8 relative group'>
-            <div style={{
-                backgroundImage: `url(${slides[currentIndex].url})`
+
+        <section
+            style={{
+                backgroundImage: 'url(/images/view1.jpeg)',
+                backgroundPosition: '50% 75%',
+                backgroundSize: '1920px',
+                backgroundRepeat: 'no-repeat',
             }}
-                className='w-full h-full rounded-lg bg-center bg-cover duration-500'>
-            
-                <div className='bg-black/60 h-full w-full flex justify-center items-center flex-col gap-2'>
-                    <h1
-                        className={`${luxurious.className} text-[6rem] md:text-[12rem] text-white leading-[2.5rem] md:leading-[5.5rem]`}
-                    >
-                        Hannah&apos;s
-                    </h1>
-                    <h2
-                        className={`${luxuriousR.className} text-[1.5rem] md:text-[3.0rem] text-white`}
-                    >
-                        Guest House
-                    </h2>
-                </div>
-
+            className='h-[50vh] lg:h-[75vh] flex justify-center items-center relative'
+        >
+            <div className='bg-black/60 h-full w-full flex justify-center items-center flex-col gap-2'>
+                <h1
+                    className={`${luxurious.className} text-[6rem] md:text-[12rem] text-white leading-[2.5rem] md:leading-[5.5rem]`}
+                >
+                    Hanna&apos;s
+                </h1>
+                <h2
+                    className={`${luxuriousR.className} text-[1.5rem] md:text-[3.0rem] text-white`}
+                >
+                    Guest House
+                </h2>
             </div>
-
-            <div className ='hidden group-hover:block absolute top-[50%] right-5 text-2xl bg-black/20 text-white rounded-full p-2  cursor-pointer'>
-                < BsChevronCompactRight onClick={nextSlide} size={30}/>
-            </div>
-            <div className='hidden group-hover:block absolute top-[50%] left-5 text-2xl bg-black/20 text-white rounded-full p-2  cursor-pointer'>
-                 < BsChevronCompactLeft onClick={prevSlide} size={30} />
-            </div>
-
-                {/* freetobook widget */}
+            {/* Quick Search Bar*/}
             <div className=' bg-white absolute md:left-[12%] lg:left-[25%] md:p-2  p-6 lg:-bottom-8 md:-bottom-12 -bottom-32 rounded-2xl md:rounded-none border-[0.1px] border-black/20 drop-shadow-md flex-row flex justify-between text-sm md:text-lg '>
                 <div
-                    className='ftb-widget md:w-[500px] lg:w-[800px]'
-                    data-id='process.env.DATA_ID'
-                    data-token='process.env.DATA_TOKEN'
+                    className='ftb-widget md:w-[600px] lg:w-[800px]'
+                    data-id='20201'
+                    data-token='R2QXxtCo5naWwHl1KSGAgE16oHBB3Xfsjlk758elAvKw2pW9GR3ClcgObUEzf'
                 ></div>
             </div>
+        </section>
 
-        </div>
+
+       
+            // <div className="className='max-w-full h-[700px] w-full m-auto pb-8 relative group'">
+
+                
+
+            // </div>
+            
+
+      
     );
 };
 
--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -2,49 +2,42 @@ import React from 'react'
 
 const Welcome = () => {
     return (
-        <div className=''>
-            <section className='py-20 md:py-36 flex items-center text-base md:text-2xl font-semibold flex-col mt-20 md:mt-0'>
-                <h1 className='  text-2xl md:text-4xl  font-semibold tracking-wider'>Welcome to Hannah&apos;s Guest House</h1>
-                <div className='flex flex-col lg:flex-row mt-10 md:mt-16 w-full'>
-                    <div className='lg:w-[50%] flex flex-col justify-center items-center px-10 md:px-20 text-justify text-base'>
-                        {' '}
-                        <p className=''> 
-                            Welcome to Hannah&apos;s Guest House! We are delighted to invite
-                            you to experience a warm and welcoming stay at our charming guest
-                            house in the heart of Aberdeen. Nestled in the vibrant
-                            neighborhood of Bon Accord, our establishment offers a delightful
-                            blend of comfort, convenience, and Scottish hospitality
-                        </p>
-                        <br />
-                        <p className=''>
-                            Whether you are a leisure traveler seeking a cozy retreat or a
-                            business traveler in need of a comfortable base, Hannah&apos;s
-                            Guest House is the perfect choice. Our dedicated team is committed
-                            to ensuring your stay is nothing short of exceptional, providing
-                            you with a home away from home. From our tastefully appointed
-                            rooms to our personalized services, we strive to create an
-                            unforgettable experience for each and every guest. Situated in
-                            close proximity to Aberdeen&apos;s renowned attractions, shopping
-                            destinations, and cultural sites, our guest house serves as an
-                            ideal starting point for exploring the beauty and rich heritage of
-                            this captivating city.
-                        </p>
-                    </div>
-                    <div
-                        className='lg:w-[50%] h-[350px] lg:h-auto flex justify-center items-center mt-10 lg:mt-0 bg-cover mx-6'
-                        style={{
-                            backgroundImage: 'url(images/outsideview1.jpeg)',
-                            backgroundPosition: '50% 75%',
-                            backgroundRepeat: 'no-repeat',
-                            height: "400px",
-                            width: "500px",
-                        }}
-                    ></div>
+        <section className='py-20 md:py-36 flex items-center text-base md:text-2xl font-semibold flex-col mt-20 md:mt-0'>
+            <h1 className='md:text-4xl'>Welcome to Hannah&apos;s Guest House</h1>
+            <div className='flex flex-col lg:flex-row mt-10 md:mt-16 w-full'>
+                <div className='lg:w-[50%] flex flex-col justify-center items-center px-10 md:px-20 text-justify text-base'>
+                    <p>
+                        Welcome to Hannah&apos;s Guest House! We are delighted to invite
+                        you to experience a warm and welcoming stay at our charming guest
+                        house in the heart of Aberdeen. Nestled in the vibrant
+                        neighborhood of Bon Accord, our establishment offers a delightful
+                        blend of comfort, convenience, and Scottish hospitality
+                    </p>
+                    <br />
+                    <p>
+                        Whether you are a leisure traveler seeking a cozy retreat or a
+                        business traveler in need of a comfortable base, Hannah&apos;s
+                        Guest House is the perfect choice. Our dedicated team is committed
+                        to ensuring your stay is nothing short of exceptional, providing
+                        you with a home away from home. From our tastefully appointed
+                        rooms to our personalized services, we strive to create an
+                        unforgettable experience for each and every guest. Situated in
+                        close proximity to Aberdeen&apos;s renowned attractions, shopping
+                        destinations, and cultural sites, our guest house serves as an
+                        ideal starting point for exploring the beauty and rich heritage of
+                        this captivating city.
+                    </p>
                 </div>
-            </section>
-           
-
-        </div>
+                <div
+                    className='lg:w-[50%] h-[350px] lg:h-auto flex justify-center items-center mt-10 lg:mt-0 bg-cover'
+                    style={{
+                        backgroundImage: 'url(/images/outsideview1.jpeg)',
+                        backgroundPosition: '50% 50%',
+                        backgroundRepeat: 'no-repeat',
+                    }}
+                ></div>
+            </div>
+        </section>
     )
 }
 
